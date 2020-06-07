22850ad0b6b85423c289b6c9de1cbb4273fe3c7d
Abiola Ibrahim <abiola89@gmail.com>
2020-06-07 07:03:35 +0100

    follow existing convention

--- linereader.go
+++ linereader.go
@@ -9,13 +9,13 @@ import (
 	"unicode"
 )
 
-var variableLineRegexp = regexp.MustCompile(`^\_([a-zA-Z0-9]+)(\s*)\:`)
+var variableLineRegexp = regexp.MustCompile(`^x\-([a-zA-Z0-9\.\_\-]+)(\s*)\:`)
 
 func commentLine(line string) bool { return strings.HasPrefix(strings.TrimSpace(line), "#") }
 func topLevelLine(line string) bool {
 	return strings.IndexFunc(line, func(r rune) bool {
 		return !unicode.IsSpace(r)
-	}) == 0 && !commentLine(line) && line[0] != '_'
+	}) == 0 && !commentLine(line) && !strings.HasPrefix(line, "x-")
 }
 func variableLine(line string) (variable string, found bool) {
 	// TODO: use other than regexp if this is notably slow.
--- marshal.go
+++ marshal.go
@@ -2,6 +2,7 @@ package caddyyaml
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/ghodss/yaml"
@@ -14,9 +15,9 @@ func yamlToJSON(b []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	// discard all entries with _ prefix
+	// discard all entries with x- prefix
 	for key := range tmp {
-		if strings.HasPrefix(key, "_") {
+		if strings.HasPrefix(key, "x-") {
 			// this is safe to do
 			// https://stackoverflow.com/a/23230406/524060
 			delete(tmp, key)
@@ -27,6 +28,7 @@ func yamlToJSON(b []byte) ([]byte, error) {
 }
 
 func varsFromBody(b []byte) (map[string]interface{}, error) {
+	var tmp map[string]interface{}
 	var vars map[string]interface{}
 
 	varsBytes, err := extractVariables(b)
@@ -39,8 +41,19 @@ func varsFromBody(b []byte) (map[string]interface{}, error) {
 		return nil, err
 	}
 
-	if err := yaml.Unmarshal(varsBytes, &vars); err != nil {
+	if err := yaml.Unmarshal(varsBytes, &tmp); err != nil {
 		return nil, err
+	}
+
+	vars = make(map[string]interface{})
+
+	// go template prohibits hyphen `-` in field names.
+	for xkey, val := range tmp {
+		key := xkey[2:] // discard x- prefix
+		if strings.Index(key, "-") > 0 {
+			return nil, fmt.Errorf("template: apart from 'x-' prefix, '-' cannot be used in extension field name for %s", xkey)
+		}
+		vars[key] = val
 	}
 
 	return vars, nil
